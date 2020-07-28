edf43b8acf4ff439448d6478f416a7ea6ea9cf68
srinivas1043 <sm1043@gmail.com>
2020-07-28 21:31:34 +0530

    first commit

---
+++ js/vivid.js
@@ -0,0 +1,5 @@
+let mainNav = document.getElementById("menu-nav");
+let navbartoggle = document.getElementById('toggle');
+navbartoggle.addEventListener('click', function() {
+    mainNav.classList.toggle('burger');
+})
