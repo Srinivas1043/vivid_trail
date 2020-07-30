b357cf7da8796e88223a2a6167951821ed892c7f
srinivas1043 <sm1043@gmail.com>
2020-07-30 19:53:43 +0530

    gallery photo add modal

--- js/vivid.js
+++ js/vivid.js
@@ -2,11 +2,4 @@ let mainNav = document.getElementById("menu-nav");
 let navbartoggle = document.getElementById('toggle');
 navbartoggle.addEventListener('click', function() {
     mainNav.classList.toggle('burger');
-})
-
-
-(function($) {
-    $(document).on('contextmenu', 'img', function() {
-        return false;
-    })
 });
